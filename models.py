f67d583068d86679e77f632e139a42005a2767a1
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-08-19 20:50:39 +0100

    Fyyur
    Fyyur Artist Booking Site

---
+++ models.py
@@ -0,0 +1,50 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ARRAY
+db = SQLAlchemy()
+from tokenize import String
+
+class Venue(db.Model):
+    __tablename__ = 'venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False, unique=True)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=True)
+    seeking_talent = db.Column(db.String(120), nullable=False)
+    seeking_description = db.Column(db.String(120), nullable=True)
+    website_link = db.Column(db.String, nullable=True)
+    shows = db.relationship('Show', backref='venue', lazy=True)
+    genres = db.Column(db.ARRAY(db.String), default=list)
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name} {self.city}>'
+
+class Artist(db.Model):
+    __tablename__ = 'artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False, unique=True)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=True)
+    seeking_venue = db.Column(db.Boolean,nullable=False,default= False)
+    seeking_description = db.Column(db.String(120), nullable=True)
+    website_link = db.Column(db.String, nullable=True)
+    shows = db.relationship('Show', backref='artist', lazy=True)
+    genres = db.Column(db.ARRAY(db.String), default=list)
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name} {self.city}>'
+
+
+class Show(db.Model):
+    __tablename__ = 'shows'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
---
+++ models/models.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from models.database import db
+
+
+@dataclass
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name: str = db.Column(db.String, nullable=False)
+    city: str = db.Column(db.String(120), nullable=False)
+    state: str = db.Column(db.String(120), nullable=False)
+    phone: str = db.Column(db.String(120), nullable=True)
+    genres: str = db.Column(db.String(120), nullable=False)
+    image_link: str = db.Column(db.String(500), nullable=True)
+    facebook_link: str = db.Column(db.String(120), nullable=True)
+    website: str = db.Column(db.String(120), nullable=True)
+    seeking_venue: bool = db.Column(db.Boolean, nullable=False)
+    seeking_description: str = db.Column(db.String(500), nullable=True)
+
+
+# *************************************************************************************
+# *************************************************************************************
+# *************************************************************************************
+
+@dataclass
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name: str = db.Column(db.String, nullable=False)
+    city: str = db.Column(db.String(120), nullable=False)
+    state: str = db.Column(db.String(120), nullable=False)
+    phone: str = db.Column(db.String(120), nullable=True)
+    image_link: str = db.Column(db.String(500), nullable=True)
+    genres: str = db.Column(db.String(120), nullable=False)
+    address: str = db.Column(db.String(120), nullable=False)
+    website: str = db.Column(db.String(120), nullable=True)
+    facebook_link: str = db.Column(db.String(120), nullable=True)
+    seeking_talent: bool = db.Column(db.Boolean, nullable=False)
+    seeking_description: str = db.Column(db.String(500), nullable=True)
+
+
+# *************************************************************************************
+# *************************************************************************************
+# *************************************************************************************
+
+
+@dataclass
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    start_time: datetime = db.Column(db.DateTime)
+
+    # Foreign keys
+    venue_id: int = db.Column(db.Integer, db.ForeignKey('Venue.id'), primary_key=True, nullable=False)
+    artist_id: int = db.Column(db.Integer, db.ForeignKey('Artist.id'), primary_key=True, nullable=False)
+
+    # Relationships
+    venue = db.relationship('Venue', backref=db.backref('shows', cascade='all, delete'))
+    artist = db.relationship('Artist', backref=db.backref('shows', cascade='all, delete'))
